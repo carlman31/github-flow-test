cbdbcad920e0846b7de572ccf3d888d91425669c
Carlos Cuellar <you@example.com>
2020-09-06 00:30:09 -0500

    Adding feature 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        feature1
+        feature2
         </p>
         <a
           className="App-link"
